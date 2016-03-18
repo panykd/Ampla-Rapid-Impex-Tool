f6ceac981f3110ba2cf36db14b4cb4a8df23fdb8
Michael Southeren <michael.southeren@gmail.com>
2016-03-18 15:14:24 +1000

    Fixed bug which cause modules on command line to be case sensitive

---
+++ RapidImpex.Testing/Features/AmplaModuleTranslation.feature
@@ -0,0 +1,13 @@
+﻿Feature: AmplaModuleTranslation
+	Ampla Modules should be able to be represented as strings, and
+	be converted backward and forward without issue.
+
+Scenario Outline: String to Module Translations
+	Given I have the string '<String>'
+	When I translate into an Ampla Module
+	Then I should have the Downtime Module
+Examples: 
+| String   |
+| Downtime |
+| downtime |
+| dOwNTime |
---
+++ RapidImpex.Testing/Features/AmplaModuleTranslationSteps.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using RapidImpex.Ampla;
+using RapidImpex.Ampla.AmplaData200806;
+using Shouldly;
+using TechTalk.SpecFlow;
+
+namespace RapidImpex.Testing.Features
+{
+    [Binding]
+    public class AmplaModuleTranslationSteps
+    {
+        private string _inputString ;
+
+        private AmplaModules? _amplaModule;
+
+        [Given(@"I have the string '(.*)'")]
+        public void GivenIHaveTheString(string value)
+        {
+            _inputString = value;
+        }
+        
+
+        [When(@"I translate into an Ampla Module")]
+        public void WhenITranslateIntoAnAmplaModule()
+        {
+            _amplaModule = _inputString.AsAmplaModule();
+        }
+        
+        [Then(@"I should have the Downtime Module")]
+        public void ThenIShouldHaveTheDowntimeModule()
+        {
+            _amplaModule.ShouldBe(AmplaModules.Downtime);
+        }
+    }
+}
